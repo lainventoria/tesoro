c5a1234fb4a70bfca50791ccc8c2c1e8f0af164d
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-08 19:57:37 -0300

    Inicial con sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cp::Application.config.secret_key_base = '36040f688fd29a1910e5664727696cdd02a03c402c95f59aaaf075d842c9c7e81bfacd6ead2624a62ef838c63bee6a1b2817ac889d7456d41f5080697e475788'
