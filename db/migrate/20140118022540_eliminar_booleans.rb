4e2a420b29e23935d44b2c99df3df5b4ecec0599
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 14:27:22 -0300

    Eliminar booleans ahora es reversible

--- db/migrate/20140118022540_eliminar_booleans.rb
+++ db/migrate/20140118022540_eliminar_booleans.rb
@@ -1,5 +1,5 @@
 class EliminarBooleans < ActiveRecord::Migration
-  def change
+  def up
     remove_column :recibos, :emitido
     remove_column :recibos, :recibido
 
@@ -8,5 +8,16 @@ class EliminarBooleans < ActiveRecord::Migration
 
     add_column :facturas, :emitida_o_recibida, :string
     add_column :recibos, :emitido_o_recibido, :string
+  end
+
+  def down
+    add_column :recibos, :emitido, :boolean
+    add_column :recibos, :recibido, :boolean
+
+    add_column :facturas, :recibida, :boolean
+    add_column :facturas, :emitida, :boolean
+
+    remove_column :facturas, :emitida_o_recibida
+    remove_column :recibos, :emitido_o_recibido
   end
 end
