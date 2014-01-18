fa4748cb808fc68e5c6d1dcd4891c28c9c9286fe
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 02:10:56 -0300

    Test preguntón

--- test/models/factura_test.rb
+++ test/models/factura_test.rb
@@ -4,4 +4,8 @@ class FacturaTest < ActiveSupport::TestCase
   test 'es un pago?' do
     assert build(:factura).pago?
   end
+
+  test 'entonces no es cobro?' do
+    assert_not build(:factura).cobro?
+  end
 end
