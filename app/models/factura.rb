0b1b2eb4eee8e910351429d9b7a902c436a36fd5
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 02:22:23 -0300

    A comentar a comentar ♬

--- app/models/factura.rb
+++ app/models/factura.rb
@@ -1,15 +1,20 @@
 class Factura < ActiveRecord::Base
+  # Las facturas se pueden cancelar con muchos recibos
   has_many :recibos
 
+  # Las situaciones posibles en que se genera una factura
   SITUACIONES = %w(cobro pago)
-
-  monetize :importe_total_centavos
+  # Sólo aceptamos esas :3
   validates_inclusion_of :situacion, in: SITUACIONES
 
+  monetize :importe_total_centavos
+
+  # Chequea si la situación es pago
   def pago?
     situacion == 'pago'
   end
 
+  # Chequea si la situación es cobro
   def cobro?
     situacion == 'cobro'
   end
--- app/models/recibo.rb
+++ app/models/recibo.rb
@@ -1,15 +1,19 @@
 class Recibo < ActiveRecord::Base
+  # Las facturas se cancelan con uno o más recibos
   belongs_to :factura
 
+  # Todas las situaciones en que se generan recibos
   SITUACIONES = %w(cobro pago)
-
-  monetize :importe_centavos
   validates_inclusion_of :situacion, in: SITUACIONES
 
+  monetize :importe_centavos
+
+  # Es un recibo de pago?
   def pago?
     situacion == 'pago'
   end
 
+  # Es un recibo de cobro?
   def cobro?
     situacion == 'cobro'
   end
