353809ca99f429042009e5da37f04816df972028
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 00:00:20 -0300

    Facturas/Recibos: entran las listas

--- app/models/factura.rb
+++ app/models/factura.rb
@@ -1,3 +1,12 @@
 class Factura < ActiveRecord::Base
   monetize :importe_total_centavos
+  validates_inclusion_of :emitida_o_recibida, in: %w(emitida recibida)
+
+  def recibida?
+    True if :emitida_o_recibida == 'recibido'
+  end
+
+  def emitida?
+    True if :emitida_o_recibida == 'emitido'
+  end
 end
--- app/models/recibo.rb
+++ app/models/recibo.rb
@@ -2,4 +2,13 @@ class Recibo < ActiveRecord::Base
   belongs_to :factura
 
   monetize :importe_centavos
+  validates_inclusion_of :emitido_o_recibido, in: %w(emitido recibido)
+
+  def recibido?
+    True if :emitido_o_recibido == 'recibido'
+  end
+
+  def emitido?
+    True if :emitido_o_recibido == 'emitido'
+  end
 end
