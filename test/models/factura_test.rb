a5c043989cf5410ae3a80cbb75f8143df21db3a5
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 01:37:19 -0300

    Test tímido

--- test/models/factura_test.rb
+++ test/models/factura_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class FacturaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'es un pago?' do
+    assert build(:factura).pago?
+  end
 end
