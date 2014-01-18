79cad7bc8bc096e9ce6d20113f5e51863fe8c8d0
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-18 00:03:48 -0300

    Reutilización de valores

--- app/models/factura.rb
+++ app/models/factura.rb
@@ -1,6 +1,9 @@
 class Factura < ActiveRecord::Base
+
+  situacion = %w(emitida recibida)
+
   monetize :importe_total_centavos
-  validates_inclusion_of :emitida_o_recibida, in: %w(emitida recibida)
+  validates_inclusion_of :emitida_o_recibida, in: situacion
 
   def recibida?
     True if :emitida_o_recibida == 'recibida'
--- app/models/recibo.rb
+++ app/models/recibo.rb
@@ -1,8 +1,10 @@
 class Recibo < ActiveRecord::Base
   belongs_to :factura
 
+  situacion = %w(emitido recibido)
+
   monetize :importe_centavos
-  validates_inclusion_of :emitido_o_recibido, in: %w(emitido recibido)
+  validates_inclusion_of :emitido_o_recibido, in: situacion
 
   def recibido?
     True if :emitido_o_recibido == 'recibido'
