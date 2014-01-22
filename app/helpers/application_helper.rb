88a63f502d7a7449e1d633ad5efc89e23104b338
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-22 16:36:41 -0300

    Layout para la aplicación con barra superior (tried and true)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Título de la página para el +<head>+ por defecto
+  def titulo_de_la_aplicacion
+    "#{titulo.present? ? "#{titulo} | " : nil}CP"
+  end
+
+  # Por defecto, no se usa nada. Cada helper específico redefine este método si
+  # quiere un título específico
+  def titulo
+    nil
+  end
 end
