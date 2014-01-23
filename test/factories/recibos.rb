bc926d2a6fb679c783ebe9aa63c278512ff30113
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-23 16:46:26 -0300

    Para que siempre se creen recibos con importe menor que el total

--- test/factories/recibos.rb
+++ test/factories/recibos.rb
@@ -2,10 +2,10 @@
 
 FactoryGirl.define do
   factory :recibo do
-    fecha "2014-01-17 21:25:25"
-    importe { Money.new rand(1000) }
-    situacion "pago"
-
     factura
+
+    fecha "2014-01-17 21:25:25"
+    importe { Money.new rand(factura.importe_total_centavos) }
+    situacion "pago"
   end
 end
