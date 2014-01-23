fb8391d7ab15d2563321aa0370137e94602c0418
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-23 17:52:19 -0300

    Test que prueba #6

--- test/models/recibo_test.rb
+++ test/models/recibo_test.rb
@@ -5,4 +5,14 @@ class ReciboTest < ActiveSupport::TestCase
     # TODO arreglar las valicadiones en recibos y facturas
     assert (r = create(:recibo)).valid?, r.errors.messages
   end
+
+  # Regresión para #6
+  test "El recibo es válido si no completa la factura" do
+    factura = create :factura, importe_total: Money.new(1000)
+    recibo = factura.recibos.build importe: Money.new(800)
+
+    assert recibo.valid?, recibo.errors.messages
+    assert recibo.save
+    assert recibo.reload.valid?, recibo.errors.messages
+  end
 end
