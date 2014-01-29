f6384c2ea0e78f5f288ce72f6ec655dbffca9280
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-29 16:46:53 -0300

    Caja va anidado en Obra

--- app/views/cajas/index.json.jbuilder
+++ app/views/cajas/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@cajas) do |caja|
   json.extract! caja, :id, :obra_id
-  json.url caja_url(caja, format: :json)
+  json.url obra_caja_url(obra, caja, format: :json)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Cp::Application.routes.draw do
   root 'obras#index'
 
+  resources :obras do
+    resources :cajas
+  end
+
   resources :cuentas
-  resources :cajas
-  resources :obras
+
   resources :facturas do
     resources :recibos
   end
+
   resources :terceros
 end
