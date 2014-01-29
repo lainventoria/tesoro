a7dc6c5cff7a0e4f740f029496c6acaf23733c44
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-29 14:34:22 -0300

    Obra tiene varias cajas, ninguna por default

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -1,8 +1,8 @@
 class Obra < ActiveRecord::Base
-  has_one :caja
+  has_many :cajas
   has_one :cuenta
 
-  after_create :create_caja, :create_cuenta
+  after_create :create_cuenta
 
   validates_presence_of :nombre, :direccion
 
--- test/models/obra_test.rb
+++ test/models/obra_test.rb
@@ -12,11 +12,4 @@ class ObraTest < ActiveSupport::TestCase
     assert_instance_of Cuenta, obra.cuenta
     assert obra.cuenta.id == obra.id
   end
-
-  test 'crea su caja asociada al crearse' do
-    obra = create :obra
-
-    assert_instance_of Caja, obra.caja
-    assert obra.caja.id == obra.id
-  end
 end
