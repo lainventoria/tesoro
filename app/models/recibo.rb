6a8b2555f612990167c90677c04c3903cb137db7
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-31 15:43:29 -0300

    Estaba pasando tests como un campeón
    * Validación de cancelación de factura al insertar recibo
    * Validación de recibo mayor al saldo de la factura
    * Tests que prueban la existencia de mpj

--- app/models/recibo.rb
+++ app/models/recibo.rb
@@ -3,6 +3,7 @@ class Recibo < ActiveRecord::Base
   belongs_to :factura, inverse_of: :recibos
   # Por eso cada recibo tiene que estar asociado a una factura
   validates_presence_of :factura
+  validate :validate_cancelacion, :validate_saldo
 
   # Todas las situaciones en que se generan recibos
   SITUACIONES = %w(cobro pago)
@@ -18,5 +19,24 @@ class Recibo < ActiveRecord::Base
   # Es un recibo de cobro?
   def cobro?
     situacion == 'cobro'
+  end
+
+  # Eso mismo
+  def validate_cancelacion
+    errors[:base] << "La factura ya fue cancelada" if factura.cancelada?
+  end
+
+  # Valida el saldo de la factura
+  def validate_saldo
+    # Si el registro es nuevo, para saber el saldo real antes de
+    # insertar tenemos que restar el importe actual
+    if self.new_record?
+      saldo = factura.saldo - importe
+    # Cuando ya está en la base directamente comprobamos el saldo
+    else
+      saldo = factura.saldo
+    end
+
+    errors[:base] << "El importe es mayor al saldo #{saldo}" if saldo < 0
   end
 end
--- test/models/recibo_test.rb
+++ test/models/recibo_test.rb
@@ -2,11 +2,9 @@ require 'test_helper'
 
 class ReciboTest < ActiveSupport::TestCase
   test "es válido" do
-    # TODO arreglar las validaciones en recibos y facturas
     assert (r = create(:recibo)).valid?, r.errors.messages
   end
 
-  # Regresión para #6
   test "El recibo es válido si no completa la factura" do
     factura = create :factura, importe_total: Money.new(1000)
     recibo = factura.recibos.build importe: Money.new(800)
@@ -17,11 +15,21 @@ class ReciboTest < ActiveSupport::TestCase
   end
 
   test "El recibo es inválido si se pasa del valor de la factura" do
-    factura = create :factura, importe_total: Money.new(1000)
-    recibo = factura.recibos.build importe: Money.new(1800)
+    factura = create :factura, importe_total: 1000
+    recibo = factura.recibos.build importe: 1800
 
-    assert_not recibo.valid?, recibo.errors.messages
+    assert recibo.invalid?, recibo.errors.messages
     assert_not recibo.save
-    assert_not recibo.reload.valid?, recibo.errors.messages
+  end
+
+  test "La factura ya fue cancelada" do
+    factura = create :factura, importe_total: 1000
+    recibo1 = factura.recibos.build importe: 1000
+    recibo2 = factura.recibos.build importe: 800
+
+    assert recibo1.valid?, recibo1.errors.messages
+    assert recibo1.save
+    assert recibo2.invalid?, recibo2.errors.messages
+    assert_not recibo2.save
   end
 end
