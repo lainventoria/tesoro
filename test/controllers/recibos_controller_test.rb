ea7e906b6df27a3fd4ab1af652750a3bcda67994
aza <aza@bender.(none)>
2014-01-31 16:22:10 -0300

    sacando ruta de recibos, arreglando tests y paths

--- test/controllers/recibos_controller_test.rb
+++ test/controllers/recibos_controller_test.rb
@@ -7,44 +7,44 @@ class RecibosControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, :factura_id => @recibo.factura.id
     assert_response :success
     assert_not_nil assigns(:recibos)
   end
 
   test "should get new" do
-    get :new
+    get :new, :factura_id => @recibo.factura.id
     assert_response :success
   end
 
   test "should create recibo" do
     assert_difference('Recibo.count') do
-      post :create, recibo: { situacion: @recibo.situacion, factura_id: @recibo.factura_id, fecha: @recibo.fecha, importe: Money.new(500) }
+      post :create, :factura_id => @recibo.factura.id, recibo: { situacion: @recibo.situacion, factura_id: @recibo.factura_id, fecha: @recibo.fecha, importe: Money.new(500) }
     end
 
-    assert_redirected_to recibo_path(assigns(:recibo))
+    assert_redirected_to factura_recibo_path(@recibo.factura,assigns(:recibo))
   end
 
   test "should show recibo" do
-    get :show, id: @recibo
+    get :show, id: @recibo, :factura_id => @recibo.factura.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @recibo
+    get :edit, id: @recibo, :factura_id => @recibo.factura.id
     assert_response :success
   end
 
   test "should update recibo" do
-    patch :update, id: @recibo, recibo: { situacion: @recibo.situacion, factura_id: @recibo.factura_id, fecha: @recibo.fecha, importe: Money.new(800) }
-    assert_redirected_to recibo_path(assigns(:recibo))
+    patch :update, id: @recibo, :factura_id => @recibo.factura.id, recibo: { situacion: @recibo.situacion, factura_id: @recibo.factura_id, fecha: @recibo.fecha, importe: Money.new(800) }
+    assert_redirected_to factura_recibo_path(@recibo.factura,assigns(:recibo))
   end
 
   test "should destroy recibo" do
     assert_difference('Recibo.count', -1) do
-      delete :destroy, id: @recibo
+      delete :destroy, id: @recibo, :factura_id => @recibo.factura.id
     end
 
-    assert_redirected_to recibos_path
+    assert_redirected_to factura_recibos_path
   end
 end
