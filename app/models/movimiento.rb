78c242f7d3f7dee82b4916534117ddf5fa01dcd1
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-02-25 19:27:02 -0300

    si se crea un movimiento sin recibo se asume que se esta creando un recibo interno

--- app/models/movimiento.rb
+++ app/models/movimiento.rb
@@ -5,4 +5,19 @@ class Movimiento < ActiveRecord::Base
   monetize :monto_centavos, with_model_currency: :monto_moneda
 
   validates_presence_of :caja, :recibo, :monto
+  before_create :crear_recibo_adhoc
+
+
+  private
+
+    # Crear un recibo para este movimiento si no se están generando
+    # movimientos a partir de un recibo
+    def crear_recibo_adhoc
+      return if not self.recibo.nil?
+
+      recibo = Recibo.create(importe: self.monto,
+                             situacion: 'interno',
+                             fecha: Time.now)
+    end
+
 end
