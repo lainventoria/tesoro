a921a728198fde2914ddeff42ae4e945c1fd4e87
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-02-28 20:54:13 -0300

    Estas vistas no hay que testearlas

--- test/controllers/cheques_controllers_test.rb
+++ test/controllers/cheques_controllers_test.rb
@@ -11,31 +11,9 @@ class ChequesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "lista de cheques vencidos" do
-    get :vencidos
-
-    assert_response :vencidos
-  end
-
-  test "accede a crear" do
-    get :new, cuenta_id: @cuenta
-
-    assert_response :success
-  end
-
-  test "crear cheque" do
-    assert_difference('Cheque.count') do
-      post :create, cuenta_id: @cuenta
-    end
-  end
-
   test "mostrar" do
     get :show, id: @cheque
 
     assert_response :success
-  end
-
-  test "actualizar" do
-    patch :update, cuenta_id: @cuenta, id: @cheque
   end
 end
