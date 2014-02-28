a753a9541bb72d7e2f2deb54572d518f5743f57b
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-02-28 10:21:49 -0300

    Testear vencimiento de los cheques

--- test/models/cheques_test.rb
+++ test/models/cheques_test.rb
@@ -4,4 +4,20 @@ class ChequeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "es valido" do
+    assert (c = create(:cheque)).valid?, c.errors.messages
+  end
+
+  test "el cheque está vencido" do
+    cheque = create :cheque, fecha_vencimiento: Time.now - 360000
+
+    assert cheque.vencido?, "#{Time.now} > #{cheque.fecha_vencimiento}"
+  end
+
+  test "el cheque no está vencido" do
+    cheque = create :cheque
+
+    assert_not cheque.vencido?, "#{Time.now} > #{cheque.fecha_vencimiento}"
+  end
 end
