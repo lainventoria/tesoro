1b80f9ae2a09755039e79553bc8407b615017032
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-03 22:42:10 -0300

    Otros eventos de facturas controlados por js #41

--- app/assets/javascripts/facturas.js
+++ app/assets/javascripts/facturas.js
@@ -17,4 +17,15 @@ $(document).ready(function(){
   $('#factura_iva').on('keyup', function() {
     $('#factura_importe_total')[0].value = parse_float($('#factura_importe_neto')[0].value) + parse_float($('#factura_iva')[0].value);
   });
+
+// al hacer click en una celda de la tabla, redirige a la vista
+// detallada no se puede aplicar al renglon entero porque sobreescribe
+// al boton de agregar recibo el id de cada celda de la lista tiene que
+// contener el path de destino
+  $('.ir-a').on('click', function() { window.location = this.id});
+
+// script que permite que un boton ubicado fuera del formulario lo
+// postee hay que referirse al formulario por su id para el caso de una
+// nueva factura el id del formulario es siempre new_factura
+  $('#btnGuardar').on('click', function() { $('#new_factura').submit(); });
 });
