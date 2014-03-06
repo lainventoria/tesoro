67fc0402e4ac4e15062a22b1d42a9b8a3cad8bfd
matus <matus@riseup.net>
2014-03-06 19:12:15 -0300

    cabios a tabla terceros
    cambia booleans por string
    agrega columnas contacto y notas

---
+++ db/migrate/20140306210751_cambios_terceros.rb
@@ -0,0 +1,21 @@
+class CambiosTerceros < ActiveRecord::Migration
+
+  def up
+    remove_column :terceros, :proveedor
+    remove_column :terceros, :cliente
+    add_column :terceros, :cliente_o_proveedor, :string
+
+    add_column :terceros, :contacto, :string
+    add_column :terceros, :notas, :text
+  end
+
+  def down
+    add_column :terceros, :proveedor, :boolean
+    add_column :terceros, :cliente, :boolean
+    remove_column :terceros, :cliente_o_proveedor
+
+    remove_column :terceros, :contacto
+    remove_column :terceros, :notas
+  end
+
+end
