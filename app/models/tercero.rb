c72a08875d4aa79b62501abf79c2f8de51a6480c
matus <matus@riseup.net>
2014-03-07 00:25:55 -0300

    ajustes a tabla terceros y primer modelo

--- app/models/tercero.rb
+++ app/models/tercero.rb
@@ -1,3 +1,21 @@
 # encoding: utf-8
 class Tercero < ActiveRecord::Base
+  include ApplicationHelper
+
+  # Cada tercero esta asociado a muchas facturas
+  has_many :facturas, inverse_of: :tercero
+
+  # Las relaciones posibles que se pueden tener con cada tercero
+  RELACIONES = %w(cliente proveedor ambos)
+
+  # Validacionesi
+  validates_inclusion_of :relacion, in: RELACIONES
+  validates_presence_of :nombre, :cuit
+  validate :cuit
+  validates_uniqueness_of :cuit
+
+  def validate_cuit
+    errors[:base] << "El CUIT no es válido" if not validar_cuit(self.cuit)
+  end
+
 end
--- db/migrate/20140306210751_cambios_terceros.rb
+++ db/migrate/20140306210751_cambios_terceros.rb
@@ -3,7 +3,7 @@ class CambiosTerceros < ActiveRecord::Migration
   def up
     remove_column :terceros, :proveedor
     remove_column :terceros, :cliente
-    add_column :terceros, :cliente_o_proveedor, :string
+    add_column :terceros, :relacion, :string
 
     add_column :terceros, :contacto, :string
     add_column :terceros, :notas, :text
@@ -12,7 +12,7 @@ class CambiosTerceros < ActiveRecord::Migration
   def down
     add_column :terceros, :proveedor, :boolean
     add_column :terceros, :cliente, :boolean
-    remove_column :terceros, :cliente_o_proveedor
+    remove_column :terceros, :relacion
 
     remove_column :terceros, :contacto
     remove_column :terceros, :notas
