e9036e0255d145a2bd0557ad91a15c450ab51b10
matus <matus@riseup.net>
2014-03-11 17:11:19 -0300

    centralizacion de scripts en assets/layout

--- app/assets/javascripts/terceros.js
+++ app/assets/javascripts/terceros.js
@@ -1,11 +1,2 @@
 $(document).ready(function(){
-// al hacer click en un renglon, redirige a la vista detallada 
-// el id de cada renglon de la lista tiene que contener el path de destino
-  $('tr').on('click', function() { window.location = this.id});
-
-// guarda el formulario de edicion/nuevo tercero
-  $('#btnGuardar').on('click', function() {
-    $('.edit_tercero').submit();
-    $('.new_tercero').submit();
-  });
 });
