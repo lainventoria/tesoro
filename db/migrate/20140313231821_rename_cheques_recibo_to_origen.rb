4ecf8ac5d8b41be839d74cbe27681c3855bbfaf2
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-13 22:43:53 -0300

    Mantenemos el nombre recibo para no tener que cambiar las vistas

--- db/migrate/20140313231821_rename_cheques_recibo_to_origen.rb
+++
@@ -1,7 +0,0 @@
-class RenameChequesReciboToOrigen < ActiveRecord::Migration
-  def change
-    remove_index :cheques, :recibo_id
-    rename_column :cheques, :recibo_id, :origen_id
-    add_index :cheques, :origen_id
-  end
-end
