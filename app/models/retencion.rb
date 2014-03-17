0e3ce0ed9abf995ebe2c7250070a7efe2fca1719
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-03-17 19:00:54 -0300

    Las retenciones sólo se asocian a facturas de pago

--- app/models/retencion.rb
+++ app/models/retencion.rb
@@ -3,11 +3,12 @@ class Retencion < ActiveRecord::Base
   # La factura sobre la que se hace la retención
   belongs_to :factura
 
+  validate :factura_es_un_pago
   # El recibo del pago de otra factura
   belongs_to :recibo
   has_one :factura_pagada, through: :recibo, source: :factura
 
-  # TODO sólo pdfs?
+  # Sólo PDFs
   has_attached_file :documento
   validates_attachment_content_type :documento, content_type: /\Aapplication\/pdf\Z/
 
@@ -21,4 +22,10 @@ class Retencion < ActiveRecord::Base
     pagar factura
     save
   end
+
+  private
+
+    def factura_es_un_pago
+      errors.add :factura, :debe_ser_un_pago unless factura.pago?
+    end
 end
--- test/models/retencion_test.rb
+++ test/models/retencion_test.rb
@@ -1,33 +1,36 @@
 require 'test_helper'
 
 class RetencionTest < ActiveSupport::TestCase
-  setup do
-    # Retención con factura para la mayoría de los tests
-    @retencion = create :retencion
-  end
-
   test 'es válida' do
     [ :build, :build_stubbed, :create].each do |metodo|
       assert_valid_factory metodo, :retencion
     end
   end
 
-  test "se usa para pagar facturas" do
-    factura = create :factura
+  test "no se puede asociar a un cobro" do
+    cobro = build :factura, situacion: 'cobro'
 
-    recibo = @retencion.pagar(factura)
+    refute build(:retencion, factura: cobro).valid?
+  end
+
+  test "se usa para pagar facturas" do
+    retencion = create :retencion
+    factura = create :factura, situacion: 'pago'
+
+    recibo = retencion.pagar(factura)
 
     assert_instance_of Recibo, recibo
-    assert_equal @retencion.monto, recibo.importe
-    assert_equal factura, @retencion.factura_pagada
+    assert_equal retencion.monto, recibo.importe
+    assert_equal factura, retencion.factura_pagada
   end
 
   test "paga y guarda" do
+    retencion = create :retencion
     factura = create :factura
 
-    @retencion.pagar(factura)
-    assert @retencion.changed?
-    @retencion.pagar!(factura)
-    refute @retencion.changed?
+    retencion.pagar(factura)
+    assert retencion.changed?
+    retencion.pagar!(factura)
+    refute retencion.changed?
   end
 end
