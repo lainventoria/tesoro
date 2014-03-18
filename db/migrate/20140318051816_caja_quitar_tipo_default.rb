4ce1d61690773454fd28888d8bd8f3d391024bf1
matus <matus@riseup.net>
2014-03-18 03:44:33 -0300

    todo: falta migrar los tipos de la vieja a la nueva columna

--- db/migrate/20140318051816_caja_quitar_tipo_default.rb
+++ db/migrate/20140318051816_caja_quitar_tipo_default.rb
@@ -1,4 +1,8 @@
 class CajaQuitarTipoDefault < ActiveRecord::Migration
+
+  # TODO ver como evitar perder los tipos existentes al borrar la
+  # antigua columna tipo y traspasarlos a la nueva columna tipo
+
   def up
     remove_column :cajas, :tipo
     add_column :cajas, :tipo, :string
