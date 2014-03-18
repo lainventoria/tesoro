33251dd8df897355d9789346de63be38fbedee5e
matus <matus@riseup.net>
2014-03-18 03:35:34 -0300

    elimina el tipo por defecto de las cajas

--- app/controllers/cajas_controller.rb
+++ app/controllers/cajas_controller.rb
@@ -31,6 +31,7 @@ class CajasController < ApplicationController
           render action: 'show', status: :created, location: @caja
         }
       else
+        params[:situacion] = @caja.situacion
         format.html { render action: 'new' }
         format.json { render json: @caja.errors, status: :unprocessable_entity }
       end
