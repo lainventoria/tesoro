dc53932afd9f0c50736b12c9b82b99ec7dea5c06
matus <matus@riseup.net>
2014-03-20 01:16:10 -0300

    nueva forma de formatear numeros que no esten en campos input

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,5 +47,9 @@ module ApplicationHelper
   def mostrar_ocultos
     true 
   end
+  
+  def formatear_numero(numero)
+    number_with_delimiter(numero, delimiter: ".", separator: ",")
+  end
 
 end
