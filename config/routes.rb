d81bc7697f06fdedfc79128cd8aeb04115911483
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-20 20:45:29 -0300

    Revert "Paths anidados"
    This reverts commit a72542430e48539e2b87c2e138874896de420324.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Cp::Application.routes.draw do
   root 'obras#index'
 
-  # facturas, recibos, cajas y cheques se modifican dentro de una obra,
-  # el resto son listados con distintos niveles de generalidad
   resources :obras do
 
-    resources :cajas do
-      resources :cheques
-    end
+    resources :cajas
 
     resources :facturas do
     # Filtrar facturas por situacion
@@ -16,38 +12,49 @@ Cp::Application.routes.draw do
         get 'pagos'
       end
 
-      # Trabajar los recibos de cada factura
+      # Ver los recibos de cada factura
       resources :recibos
     end
+
+    # Permitir /recibos pero no crear recibos sin facturas asociadas
+    resources :recibos, except: [ :new ] do
+      collection do
+        get 'cobros'
+        get 'pagos'
+      end
+    end
+
+    resources :cheques, only: [ :index, :show ] do
+      collection do
+        get 'propios'
+        get 'terceros'
+      end
+    end
   end
 
-  resources :cajas, only: [ :index, :show ] do
-    resources :cheques, only: [ :index, :show ]
-  end
+  resources :cajas
 
-  # ver las facturas y filtrarlas por situacion
-  resources :facturas, only: [ :index, :show ] do
+  resources :facturas, except: [ :index ] do
+  # Filtrar facturas por situacion
     collection do
       get 'cobros'
       get 'pagos'
     end
 
-    # Permitir ver los /recibos de esta factura
-    resources :recibos, only: [ :index, :show ]
+    # Ver los recibos de cada factura
+    resources :recibos
   end
 
-  # ver /recibos y filtrarlos por situacion
-  resources :recibos, only: [ :index, :show ] do
+  # Permitir /recibos pero no crear recibos sin facturas asociadas
+  resources :recibos, except: [ :new, :index, :create ] do
     collection do
       get 'cobros'
       get 'pagos'
     end
   end
 
-  # los terceros se crean independientemente de las obras
   resources :terceros
 
-  # ver /cheques y filtrarlos por situacion
   resources :cheques, only: [ :index, :show ] do
     collection do
       get 'propios'
