c144f165d032da1a293af0717e260839ebbf7be8
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-03-23 18:02:48 -0300

    Modelo de pagos no trackeables

---
+++ test/models/pago_no_trackeable_test.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require 'test_helper'
+
+# Testear sarasa!
+class Sarasa < PagoNoTrackeable
+end
+
+class PagoNoTrackeableTest < ActiveSupport::TestCase
+  test 'registra el monto en pesos por default' do
+    assert_equal Money.new(100), Sarasa.new(monto: Money.new(100)).monto
+  end
+
+  test 'registra el monto' do
+    assert_equal Money.new(100, 'USD'),
+      Sarasa.new(monto: Money.new(100, 'USD')).monto
+  end
+
+  test 'el id es siempre el mismo' do
+    assert_equal 1, Sarasa.new.id
+  end
+
+  test 'registra la caja a la que pertenece el movimiento de pago' do
+    caja = create(:caja)
+    assert_equal caja, Sarasa.new(caja: caja).caja
+  end
+end
