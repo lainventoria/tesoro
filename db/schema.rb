d71548b0e5a4187bcaab10fd2f87fdb2fce7eb6c
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-24 16:36:16 -0300

    asociar facturas con obras al azar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318231717) do
+ActiveRecord::Schema.define(version: 20140324191746) do
 
   create_table "cajas", force: true do |t|
     t.integer  "obra_id"
