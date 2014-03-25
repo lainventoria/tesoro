d3e289b0b959a0aee398e574d27e5efbe8a48383
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-24 21:12:45 -0300

    Calcular y mostrar saldos por obra

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -7,6 +7,33 @@ class Obra < ActiveRecord::Base
 
   validates_presence_of :nombre, :direccion
 
+  # Sumar los saldos de todas las facturas según situación
+  def saldo_de(pago_o_cobro)
+    saldo = Money.new(0)
+
+    facturas.where(situacion: pago_o_cobro).find_each do |factura|
+      saldo += factura.saldo
+    end
+
+    saldo
+  end
+
+  # los pagos son salidas
+  # TODO si pasamos a registrar los movimientos de pago como movimientos
+  # negativos hay que cambiar acá
+  def saldo_de_pago
+    saldo_de('pago') * -1
+  end
+
+  def saldo_de_cobro
+    saldo_de 'cobro'
+  end
+
+  # positivo + negativo es lo mismo que positivo - positivo :P
+  def saldo_general
+    saldo_de_cobro + saldo_de_pago
+  end
+
   private
 
     def crear_cajas
--- test/models/obra_test.rb
+++ test/models/obra_test.rb
@@ -16,4 +16,15 @@ class ObraTest < ActiveSupport::TestCase
     assert_equal 1, obra.cajas.where(tipo: 'Caja de Ahorro').count
     assert_equal 1, obra.cajas.where(tipo: 'Chequera').count
   end
+
+  test 'chequear los saldos' do
+    obra = create :obra
+    5.times { obra.facturas.create(situacion: 'pago', importe_neto: Money.new(1000), iva: Money.new(210)) }
+    5.times { obra.facturas.create(situacion: 'cobro', importe_neto: Money.new(1000), iva: Money.new(210)) }
+
+    assert_equal Money.new((1000 + 210) * -5), obra.saldo_de_pago
+    assert_equal Money.new((1000 + 210) *  5), obra.saldo_de_cobro
+    assert_equal Money.new(0), obra.saldo_general
+  end
+
 end
