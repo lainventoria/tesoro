f4cb62e17fac4771299dac4428a8d3e6c230da02
matus <matus@riseup.net>
2014-03-24 22:07:58 -0300

    selector de moneda y script

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -20,15 +20,22 @@ $(document).ready(function(){
   // redirige al path almacenado en data-uri
   // ej: se utiliza en listados para redirigir a vista detallada 
   $(document).on('click', '.ir-a', function(e) {
-      if ( $(e.target).data('uri') ) {
-        window.location.href = $(e.target).data('uri');
-        return false;
-      }
+    if ( $(e.target).data('uri') ) {
+      window.location.href = $(e.target).data('uri');
+      return false;
+    }
+    if ( $(e.target.parentElement).data('uri') ) {
+      window.location.href = $(e.target.parentElement).data('uri');
+      return false;
+    }
+  });
 
-      if ( $(e.target.parentElement).data('uri') ) {
-        window.location.href = $(e.target.parentElement).data('uri');
-        return false;
-      }
+  // actualiza campos con clase 'actualizar_moneda' con lo que se elija
+  // en el selector incluido en el partial 'layouts/selector_moneda'
+  $('#selector_moneda').on('change', function() {
+    $('.actualizar_moneda').each( function() {
+      this.value = $('#selector_moneda')[0].value;
+    });
   });
 
 });
