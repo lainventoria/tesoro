5556c7181a2f67728a6c3d67597d22f1a4438b90
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-03-28 08:45:42 -0300

    Traer partials de pagos con js

--- app/helpers/recibos_helper.rb
+++ app/helpers/recibos_helper.rb
@@ -1,6 +1,5 @@
 module RecibosHelper
-  
-  def recibos_por_tipo_path  
+  def recibos_por_tipo_path
     if @recibo.pago?
       pagos_recibos_path
     else
@@ -8,4 +7,17 @@ module RecibosHelper
     end
   end
 
+  def link_a_causa(movimiento)
+    case movimiento.causa_type
+      when 'Efectivo', 'Transferencia'
+        movimiento.causa_type
+      else
+        link_to(movimiento.causa)
+    end
+  end
+
+  def link_a_nuevo_movimiento(causa)
+    link_to causa, new_causa_path(causa: { tipo: causa.parameterize }, obra_id: @recibo.obra),
+      data: { remote: true }, class: 'agregar-movimiento'
+  end
 end
