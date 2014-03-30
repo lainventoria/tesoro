739886463b1afeb461a6dcd82d0ce855284d3de0
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-03-30 17:07:00 -0300

    Si hay obra cargar los a través suyo (arregla #91)

--- test/controllers/facturas_controller_test.rb
+++ test/controllers/facturas_controller_test.rb
@@ -5,12 +5,12 @@ class FacturasControllerTest < ActionController::TestCase
     @factura = create :factura
   end
 
-  test "should get new" do
+  test "accede a nueva factura" do
     get :new
     assert_response :success
   end
 
-  test "should create factura" do
+  test "crea" do
     assert_difference('Factura.count') do
       post :create, factura: { tercero: @factura.tercero, descripcion: @factura.descripcion, situacion: @factura.situacion, fecha: @factura.fecha, fecha_pago: @factura.fecha_pago, importe_total: @factura.importe_total, iva: @factura.iva, numero: @factura.numero, tipo: @factura.tipo }
     end
@@ -18,23 +18,34 @@ class FacturasControllerTest < ActionController::TestCase
     assert_redirected_to factura_path(assigns(:factura))
   end
 
-  test "should show factura" do
+  test "muestra" do
     get :show, id: @factura
     assert_response :success
   end
 
-  test "should get edit" do
+  test "muestra a través de su obra" do
+    get :show, obra_id: @factura.obra, id: @factura
+    assert_response :success
+  end
+
+  test "no muestra a través de otra obra" do
+    # Rails devuelve un 404 por default con estos errores en producción
+    assert_raise ActiveRecord::RecordNotFound do
+      get :show, obra_id: create(:obra), id: @factura
+    end
+  end
+
+  test "accede a editar" do
     get :edit, id: @factura
     assert_response :success
   end
 
-  test "should update factura" do
+  test "actualiza" do
     patch :update, id: @factura, factura: { tercero: @factura.tercero, descripcion: @factura.descripcion, situacion: @factura.situacion, fecha: @factura.fecha, fecha_pago: @factura.fecha_pago, importe_total: @factura.importe_total, iva: @factura.iva, numero: @factura.numero, tipo: @factura.tipo }
     assert_redirected_to factura_path(assigns(:factura))
   end
 
-  test "should destroy factura" do
-    
+  test "destruye" do
     ruta = if @factura.pago?
       pagos_facturas_path
     else
