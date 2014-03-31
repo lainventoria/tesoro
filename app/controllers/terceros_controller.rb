d9f3950aab289e0b8ed1db372dddbd41b0049db5
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-03-31 19:05:09 -0300

    no borrar terceros si tiene facturas asociadas

--- app/controllers/terceros_controller.rb
+++ app/controllers/terceros_controller.rb
@@ -58,10 +58,16 @@ class TercerosController < ApplicationController
   # DELETE /terceros/1
   # DELETE /terceros/1.json
   def destroy
-    @tercero.destroy
-    respond_to do |format|
-      format.html { redirect_to terceros_url }
-      format.json { head :no_content }
+    if @tercero.destroy
+      respond_to do |format|
+        format.html { redirect_to terceros_url }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { render action: 'edit' }
+        format.json { head :no_content }
+      end
     end
   end
 
