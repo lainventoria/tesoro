92fb10d9af0a214dbc4d4e3761cd5b3881480608
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-04-01 06:17:54 -0300

    Arreglo el filestyle para que se muestre y muestre el link al documento existente

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -44,7 +44,6 @@ $(document).ready(function(){
     classInput: 'filestyle form-control',
     classButton: 'filestyle btn btn-primary',
     classIcon: 'glyphicon glyphicon-paperclip',
-    input: false
   });
 
 });
