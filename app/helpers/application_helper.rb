351ac246416203662dee0072b36db60fa660aa68
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-02 17:40:46 -0300

    etiquetas de situacion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -125,4 +125,13 @@ module ApplicationHelper
     html
   end
 
+  def etiqueta_de_situacion(situacion)
+    case situacion
+      when 'pago' then 'label-primary'
+      when 'cobro' then 'label-success'
+      when 'propio' then 'label-primary'
+      when 'terceros' then 'label-info'
+      else 'label-default'
+    end
+  end
 end
