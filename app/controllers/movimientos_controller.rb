87d7dd5a85b0de7b7b6ec18c01bbe8b6efc88ba1
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-04-02 02:45:14 -0300

    Usar la api

--- app/controllers/movimientos_controller.rb
+++ app/controllers/movimientos_controller.rb
@@ -5,19 +5,18 @@ class MovimientosController < ApplicationController
   before_action :set_factura
 
   def new
-    # FIXME varias retenciones
-    @causa = if params[:causa_tipo] == 'retencion'
-      @factura.retencion
-    else
-      causa.try :new
-    end
-
     respond_to do |format|
-      if @causa.present?
-        format.js { render partial: "causas/#{causa_conocida}", layout: false }
+      if modelo_de_causa.present?
+        format.js { render partial: partial_segun_causa, layout: false }
       else
         render status: 404
       end
     end
   end
+
+  private
+
+    def partial_segun_causa
+      modelo_de_causa.name.underscore
+    end
 end
