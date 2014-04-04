11390adfc97c43b50e97b49663ca855fe728884a
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-04 20:32:59 -0300

    scopear las cajas sin tipo_factura

--- test/models/obra_test.rb
+++ test/models/obra_test.rb
@@ -11,16 +11,20 @@ class ObraTest < ActiveSupport::TestCase
   test 'crea sus cajas asociadas al crearse' do
     obra = create :obra
 
-    assert_equal 10, obra.cajas.count
+    # las cajas X van a ser ignoradas
+    assert_equal 8, obra.cajas.count
     assert_equal 1, obra.cajas.where(tipo: 'Obra').count
     assert_equal 1, obra.cajas.where(tipo: 'Administración').count
-    assert_equal 1, obra.cajas.where(tipo: 'Administración X').count
     assert_equal 1, obra.cajas.where(tipo: 'Seguridad').count
     assert_equal 1, obra.cajas.where(tipo: 'Caja de Ahorro').count
     assert_equal 1, obra.cajas.where(tipo: 'Chequera').count
     assert_equal 1, obra.cajas.where(tipo: 'Chequera propia').count
-    assert_equal 1, obra.cajas.where(tipo: 'Chequera X').count
     assert_equal 1, obra.cajas.where(tipo: 'Retenciones de Ganancias').count
     assert_equal 1, obra.cajas.where(tipo: 'Retenciones de Cargas Sociales').count
+    # pero se pueden llamar aparte especificamente
+    assert_equal 2, obra.cajas.where(tipo_factura: 'X').count
+
+    assert_equal 1, obra.cajas.where(tipo: 'Administración X', tipo_factura: 'X').count
+    assert_equal 1, obra.cajas.where(tipo: 'Chequera X', tipo_factura: 'X').count
   end
 end
