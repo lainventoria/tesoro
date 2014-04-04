70e4b582f4b039e1ff2d761e69dddf5781197fa7
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-03 21:00:59 -0300

    cajas en negro

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -89,12 +89,13 @@ class Obra < ActiveRecord::Base
   private
 
     def crear_cajas
-      ['Obra', 'Administración', 'Seguridad'].each do |tipo|
+      ['Obra', 'Administración', 'Administración en Negro', 'Seguridad'].each do |tipo|
         cajas.create tipo: tipo, situacion: 'efectivo'
       end
 
       cajas.create tipo: 'Caja de Ahorro', situacion: 'banco'
       cajas.create tipo: 'Chequera', situacion: 'chequera'
+      cajas.create tipo: 'Chequera en Negro', situacion: 'chequera'
       cajas.create tipo: 'Chequera propia', situacion: 'chequera'
       cajas.create tipo: 'Retenciones de Ganancias', situacion: 'chequera'
       cajas.create tipo: 'Retenciones de Cargas Sociales', situacion: 'chequera'
--- test/models/obra_test.rb
+++ test/models/obra_test.rb
@@ -11,13 +11,15 @@ class ObraTest < ActiveSupport::TestCase
   test 'crea sus cajas asociadas al crearse' do
     obra = create :obra
 
-    assert_equal 8, obra.cajas.count
+    assert_equal 10, obra.cajas.count
     assert_equal 1, obra.cajas.where(tipo: 'Obra').count
     assert_equal 1, obra.cajas.where(tipo: 'Administración').count
+    assert_equal 1, obra.cajas.where(tipo: 'Administración en Negro').count
     assert_equal 1, obra.cajas.where(tipo: 'Seguridad').count
     assert_equal 1, obra.cajas.where(tipo: 'Caja de Ahorro').count
     assert_equal 1, obra.cajas.where(tipo: 'Chequera').count
     assert_equal 1, obra.cajas.where(tipo: 'Chequera propia').count
+    assert_equal 1, obra.cajas.where(tipo: 'Chequera en Negro').count
     assert_equal 1, obra.cajas.where(tipo: 'Retenciones de Ganancias').count
     assert_equal 1, obra.cajas.where(tipo: 'Retenciones de Cargas Sociales').count
   end
