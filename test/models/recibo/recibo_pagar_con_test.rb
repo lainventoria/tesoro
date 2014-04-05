79ef29e27b87078c965365195cdf397205285c34
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-05 12:39:53 -0300

    las facturas X con las cajas X

--- test/models/recibo/recibo_pagar_con_test.rb
+++ test/models/recibo/recibo_pagar_con_test.rb
@@ -13,4 +13,20 @@ class ReciboPagarConTest < ActiveSupport::TestCase
 
     assert recibo.pagar_con(medio_de_pago)
   end
+
+  test "no pagar facturas y en cajas x" do
+    recibo = create :recibo, factura: @factura
+    medio_de_pago = Efectivo.new monto: @factura.importe_total,
+      caja: create(:caja, :con_fondos, monto: @factura.importe_total, tipo_factura: 'X')
+
+    assert_not recibo.pagar_con(medio_de_pago)
+  end
+
+  test "pagar facturas x en cajas x" do
+    recibo = create :recibo, factura: create(:factura, situacion: 'pago', tipo: 'X')
+    medio_de_pago = Efectivo.new monto: @factura.importe_total,
+      caja: create(:caja, :con_fondos, monto: @factura.importe_total, tipo_factura: 'X')
+
+    assert recibo.pagar_con(medio_de_pago)
+  end
 end
