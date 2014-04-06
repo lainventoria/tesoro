11edb5875e239abd708346b68956913de7f44ad8
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-04-06 19:00:53 -0300

    Muchísimo saldo en los tests por default

--- test/factories/facturas.rb
+++ test/factories/facturas.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     numero "MyString"
     situacion "pago"
     descripcion "MyText"
-    importe_neto { Money.new (1000 + rand(1000)) }
+    importe_neto { Money.new (10000000 + rand(1000)) }
     iva { Money.new(importe_neto * 0.21) }
     fecha "2014-01-17 20:21:17"
     fecha_pago "2014-01-17 20:21:17"
