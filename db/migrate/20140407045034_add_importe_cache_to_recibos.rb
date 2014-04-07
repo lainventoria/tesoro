8d1c81a0535a086d3a6007ab2011843a4a780c20
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-04-07 02:06:56 -0300

    faltaba setear la cache para los recibos preexistentes

--- db/migrate/20140407045034_add_importe_cache_to_recibos.rb
+++ db/migrate/20140407045034_add_importe_cache_to_recibos.rb
@@ -1,6 +1,15 @@
 class AddImporteCacheToRecibos < ActiveRecord::Migration
   def up
     add_money :recibos, :importe_cache 
+
+    reversible do |dir|
+      dir.up {
+        Recibo.find_each do |r|
+          r.importe_cache = r.importe
+          r.save
+        end
+      }
+    end
   end
 
   def down
