071a7e4181de9d53dd24891221394964833cf1d3
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-05-28 00:19:49 -0300

    cuotas: algunos tests y validaciones

--- app/models/cuota.rb
+++ app/models/cuota.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 class Cuota < ActiveRecord::Base
   monetize :monto_original_centavos, with_model_currency: :monto_original_moneda
+
+  validates_numericality_of :monto_original_centavos, greater_than: 0
+  validates_presence_of :vencimiento
+  validates_presence_of :descripcion
 end
 
