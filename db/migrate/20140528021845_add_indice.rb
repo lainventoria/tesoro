5858133693045941866da3b530ea73272359582c
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-05-27 23:24:31 -0300

    indices: crear tabla de indices

---
+++ db/migrate/20140528021845_add_indice.rb
@@ -0,0 +1,12 @@
+class AddIndice < ActiveRecord::Migration
+  def change
+    create_table :indices do |t|
+      t.timestamps
+
+      t.date      :periodo
+      t.string    :denominacion
+      t.decimal   :indice
+
+    end
+  end
+end
