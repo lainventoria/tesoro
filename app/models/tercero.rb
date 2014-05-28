216b79f733a122172ba9f8c14acab2dfb094144a
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-05-27 21:54:51 -0300

    Reingeniería de Tercero

--- app/models/tercero.rb
+++ app/models/tercero.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 class Tercero < ActiveRecord::Base
-  include ApplicationHelper
-
   # Cada tercero esta asociado a muchas facturas
   has_many :facturas, inverse_of: :tercero, dependent: :restrict_with_error
 
@@ -17,9 +15,30 @@ class Tercero < ActiveRecord::Base
   # data dummy porque todos los cuits son iguales
   #  validates_uniqueness_of :cuit
 
+  # seguramente hay una forma más elegante de hacer esto...
+  def self.validar_cuit(cuit)
+    # convertir a string si se pasa un número, remover los guiones si
+    # era una cadena
+    cuit_sin_validar = cuit.to_s.gsub /[^0-9]/, ''
+
+    # parece que el cuit es siempre de 11 cifras
+    return nil if not cuit_sin_validar.length == 11
+
+    multiplicadores = [ 5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 ]
+    resultado = 0
+
+    # multiplica cada elemento del cuit por uno de los multiplicadores
+    for i in 0..10
+      resultado = resultado + cuit_sin_validar[i].to_i * multiplicadores[i]
+    end
+
+    # el cuit es valido si el resto de dividir el resultado por 11 es 0
+    (resultado % 11) == 0
+  end
+
   # Valida la validez del CUIT 
   def validate_cuit
-    errors[:base] << "El CUIT no es válido" if not validar_cuit(self.cuit)
+    errors.add(:cuit, :no_es_valido) unless cuit_valido?
   end
 
   # Es un proveedor?
@@ -31,5 +50,8 @@ class Tercero < ActiveRecord::Base
   def cliente?
     relacion != "proveedor"
   end
-  
+
+  def cuit_valido?
+    Tercero.validar_cuit(cuit)
+  end
 end
