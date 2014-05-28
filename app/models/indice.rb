02797b206274a4c53b3db3539ac16a7bc7f51cc7
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-05-27 23:46:52 -0300

    indices: todos los atributos son necesarios

--- app/models/indice.rb
+++ app/models/indice.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 class Indice < ActiveRecord::Base
+  validates_presence_of :periodo
+  validates_presence_of :denominacion
+  validates_presence_of :valor
 end
--- test/models/indice.rb
+++ test/models/indice.rb
@@ -3,8 +3,23 @@ require 'test_helper'
 
 class IndiceTest < ActiveSupport::TestCase
   test "es válido" do
-    [ :build, :build_stubbed, :create].each do |metodo|
+    [ :build, :build_stubbed, :create ].each do |metodo|
       assert_valid_factory metodo, :indice
     end
+  end
+
+  test "necesita un periodo" do
+    assert i = build(:indice, periodo: nil)
+    assert_not i.save
+  end
+
+  test "necesita una denominacion" do
+    assert i = build(:indice, denominacion: nil)
+    assert_not i.save
+  end
+
+  test "necesita un valor" do
+    assert i = build(:indice, valor: nil)
+    assert_not i.save
   end
 end
