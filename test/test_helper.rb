1a252a937584dd2dc9656298f1a9f1181fd71082
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-05-28 23:00:41 -0300

    Limpiar la BD después de cada test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,26 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/mock'
+require 'database_cleaner'
+
+# Borrar todas las tablas de la DB
+DatabaseCleaner.clean_with :truncation
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-  # Add more helper methods to be used by all tests here...
-
-  # permite usar create, build, etc directamente
+  # Permite usar create, build, etc directamente
   include FactoryGirl::Syntax::Methods
+
+  ActiveRecord::Migration.check_pending!
+
+  setup do
+    # Iniciar la transacción
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    # Rollbackear así el siguiente test está con estado limpio
+    DatabaseCleaner.clean
+  end
 
   # Prueba que la fábrica construya objetos válidos, :build por default
   def assert_valid_factory(metodo, tipo)
