3f2b28b28695f5cf602a856784579ce8e4166ae2
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-03 09:56:22 -0300

    cuotas: comentarios

--- app/models/cuota.rb
+++ app/models/cuota.rb
@@ -3,7 +3,6 @@ class Cuota < ActiveRecord::Base
   monetize :monto_original_centavos, with_model_currency: :monto_original_moneda
 
   validates_numericality_of :monto_original_centavos, greater_than: 0
-  validates_presence_of :vencimiento
-  validates_presence_of :descripcion
+  validates_presence_of :vencimiento, :descripcion
 end
 
