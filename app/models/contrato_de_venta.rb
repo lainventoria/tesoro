7ccfb857d7513a9aa821d3172388d3227ac14873
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-04 19:40:45 -0300

    contrato_de_venta: con el indice bastaba

--- app/models/contrato_de_venta.rb
+++ app/models/contrato_de_venta.rb
@@ -4,7 +4,7 @@ class ContratoDeVenta < ActiveRecord::Base
   has_one  :tercero
   has_many :cuotas
 
-  validates_presence_of :indice_id, :tercero_id, :monto_total_centavos, :indice_base
+  validates_presence_of :indice_id, :tercero_id, :monto_total_centavos
 
   monetize :monto_total_centavos, with_model_currency: :monto_total_moneda
 end
--- test/factories/contratos_de_venta.rb
+++ test/factories/contratos_de_venta.rb
@@ -6,9 +6,7 @@ FactoryGirl.define do
 
     fecha { Time.now }
 
-    # que el indice sea un float
-    monto_total { rand(2000) + rand(99) / 100.0 }
-    indice_base { indice.indice }
+    monto_total { Money.new (10000000 + rand(1000)) }
   end
 end
 
