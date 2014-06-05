fd88e52dcfefab936e05e22718f2736b212656af
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-04 21:29:12 -0300

    contratos_de_venta: cálculo de cuotas closes #223

--- app/models/contrato_de_venta.rb
+++ app/models/contrato_de_venta.rb
@@ -15,7 +15,39 @@ class ContratoDeVenta < ActiveRecord::Base
     self.cuotas.create(attributes)
   end
 
+  # crea un pago inicial con la fecha de vencimiento igual a la fecha
+  # del contrato
+  # TODO chequear que sea el único?
   def hacer_pago_inicial(monto)
     crear_cuota(descripcion: 'Pago inicial', vencimiento: fecha, monto_original: monto)
+  end
+
+  # divide el monto total menos el pago inicial en partes iguales
+  def crear_cuotas(cantidad)
+    # antes hay que hacer un pago inicial
+    return false if cuotas.empty?
+
+    monto_a_financiar = monto_total - cuotas.first.monto_original
+    # esto va a sobrar de la división y se lo mandamos a la primera
+    # cuota
+    resto = monto_a_financiar % cantidad
+    monto_por_cuota = monto_a_financiar / cantidad
+
+    # crear las cuotas mensuales
+    Cuota.transaction do
+      cantidad.times { |cuota|
+        cuota+=1
+        crear_cuota(monto_original: monto_por_cuota,
+          vencimiento: fecha + cuota.months,
+          descripcion: "Cuota ##{cuota}")
+      }
+
+      # sumarle el resto a la primera cuota
+      if resto > 0
+        cuotas.second.monto_original += Money.new(resto)
+        cuotas.second.save
+      end
+    end
+
   end
 end
--- test/models/contrato_de_venta.rb
+++ test/models/contrato_de_venta.rb
@@ -15,4 +15,14 @@ class ContratoDeVentaTest < ActiveSupport::TestCase
     assert_equal 'Pago inicial', cv.cuotas.first.descripcion
     assert_equal Money.new(1000 * 100), cv.cuotas.first.monto_original
   end
+
+  test "el contrato tiene cuotas" do
+    cv = create(:contrato_de_venta, monto_total: Money.new(100000 * 100))
+
+    assert cv.hacer_pago_inicial(Money.new(900 * 100))
+    assert cv.crear_cuotas(12)
+
+    assert_equal 13, cv.cuotas.count
+    assert_equal cv.monto_total, Money.new(cv.cuotas.pluck(:monto_original_centavos).sum)
+  end
 end
