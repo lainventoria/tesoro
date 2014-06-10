700856823da89a14412c8a021a5f0c5d12cb2dee
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-10 20:55:45 -0300

    cuotas: reingenieria de test

--- test/models/cuota.rb
+++ test/models/cuota.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class CuotaTest < ActiveSupport::TestCase
+  setup do
+    @indice = create(:indice, valor: 1100, periodo: '2014-05-01')
+    @cv = create(:contrato_de_venta, indice: @indice, fecha: '2014-05-01')
+
+    @cv.valid?
+    @cv.hacer_pago_inicial(@cv.monto_total * 0.1)
+    @cv.crear_cuotas(12)
+  end
+
   test "es válida" do
     [ :build, :build_stubbed, :create].each do |metodo|
       assert_valid_factory metodo, :cuota
@@ -23,28 +32,20 @@ class CuotaTest < ActiveSupport::TestCase
   end
 
   test "el monto se actualiza en base al indice actual" do
-    assert indice = create(:indice, valor: 1100, periodo: '2014-05-01')
     assert indice_siguiente = create(:indice, valor: 1200, periodo: '2014-06-01')
 
-    assert cv = create(:contrato_de_venta, indice: indice, fecha: '2014-05-01')
-    assert cv.valid?
-    assert cv.hacer_pago_inicial(cv.monto_total * 0.1)
-    assert cv.crear_cuotas(2)
-
-    assert cuota = cv.cuotas.where(vencimiento: '2014-06-01').first
+    assert cuota = @cv.cuotas.where(vencimiento: '2014-06-01').first
 
     assert_equal indice_siguiente, cuota.indice_actual
-    assert_equal cuota.monto_original * (indice_siguiente.valor / indice.valor), cuota.monto_actualizado
+    assert_equal cuota.monto_original * (indice_siguiente.valor / @indice.valor), cuota.monto_actualizado
   end
 
   test "listar cuotas vencidas" do
-    assert cv = create(:contrato_de_venta)
-    assert cv.valid?
-    assert cv.hacer_pago_inicial(cv.monto_total * 0.1)
-    assert cv.crear_cuotas(12)
-
     # todas las cuotas vencidas de acá a 5 meses
-    assert_equal 5, Cuota.vencidas(Time.now + 5.months).count
+    assert_equal 5, Cuota.vencidas('2014-05-01'.to_time + 5.months).count
+  end
+
+  test "cuando las cuotas se vencen se genera una factura" do
   end
 
 end
