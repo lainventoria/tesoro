52fa8d1f3de52135ea3116525dd05099045f5b48
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-13 20:15:16 -0300

    indices_controller con sus tests, closes #233

---
+++ test/controllers/indices_controller_test.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+require 'test_helper'
+
+class IndicesControllerTest < ActionController::TestCase
+  setup do
+    @indice = create :indice
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:indices)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create indice usando un cuit diferente" do
+    assert_difference('Indice.count') do
+      post :create, indice: { denominacion: @indice.denominacion, periodo: @indice.periodo, valor: @indice.valor }
+    end
+
+    assert_redirected_to indice_path(assigns(:indice))
+  end
+
+  test "should show indice" do
+    get :show, id: @indice
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @indice
+    assert_response :success
+  end
+
+  test "should update indice" do
+    patch :update, id: @indice, indice: { denominacion: @indice.denominacion, periodo: @indice.periodo, valor: @indice.valor }
+    assert_redirected_to indice_path(assigns(:indice))
+  end
+
+  test "should destroy indice" do
+    assert_difference('Indice.count', -1) do
+      delete :destroy, id: @indice
+    end
+
+    assert_redirected_to indices_path
+  end
+
+end
