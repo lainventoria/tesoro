0b6beb575297397290165ab390a1be94287f2656
aza <aza@esfriki.com>
2014-06-16 01:22:37 -0300

    poder agregar unidades funcionales al contrato

--- app/assets/javascripts/contratos_de_venta.js
+++ app/assets/javascripts/contratos_de_venta.js
@@ -26,6 +26,37 @@ $(document).ready(function(){
     return false;
   });
 
+  // Agregar unidad x id
+  var agregar_unidad = function(id) {
+    $obra = $('input[name=obra_id]');
+    $.get('/obras/' + $obra.val() + '/unidades_funcionales/' + id +'.json', function(data) {
+      html = '<tr><td>' + data.tipo + ' ' + data.id + '</td>'
+      html += '<td>' + data.precio_venta_moneda + '</td>';
+      html += '<td><input type="text" data-role="money" name="unidades_funcionales[' + data.id + '][precio_venta]" value="' + data.precio_venta_centavos / 100 + '" class="form-control precio" /></td><td></td></tr>';
+      $('#contrato_de_venta_unidades_funcionales_table').append(html);
 
+      $('input[data-role=money]').autoNumeric('init');
+      actualizar_total();
+
+    }, 'json');
+  }
+
+
+  var actualizar_total = function() {
+    var sum = 0;
+    $('.precio').each(function(){
+      sum += Number($(this).autoNumeric('get'));
+    });
+    $('#contrato_de_venta_total').autoNumeric('set',sum);
+  }
+
+  $('#contrato_de_venta_unidades_funcionales_table').on('keyup','.precio',actualizar_total);
+
+  // Agregar unidad seleccionada
+  $('#contrato_de_venta_unidad_funcional_agregar').on('click',function(e){
+    unidad_id = $('#contrato_de_venta_unidades_funcionales').val();
+    agregar_unidad(unidad_id);
+    $('#contrato_de_venta_unidades_funcionales option:selected').remove();
+  });
 
 });
