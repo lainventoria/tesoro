c1a27c2c709242fab900ac68f40bba29bc52314f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-07-09 21:47:05 -0300

    Configuración general producción

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,18 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'cp'
+set :repo_url, 'git@github.com:lainventoria/cp.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
+# Defaults de capistrano que usamos
+# set :branch, :master
 # set :scm, :git
-
-# Default value for :format is :pretty
 # set :format, :pretty
-
-# Default value for :log_level is :debug
 # set :log_level, :debug
+# set :pty, false
+# set :keep_releases, 5
 
-# Default value for :pty is false
-# set :pty, true
+set :rbenv_ruby, '2.0.0-p353'
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
@@ -31,16 +23,13 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      # Para reiniciar passenger
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -49,9 +38,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
 
