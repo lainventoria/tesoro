17ce14463358c9c27dc2d177e08f1ccea226dc88
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-07-10 22:42:28 -0300

    Migraciones reversibles

--- db/migrate/20140603125346_cuotas_not_null.rb
+++ db/migrate/20140603125346_cuotas_not_null.rb
@@ -1,6 +1,11 @@
 class CuotasNotNull < ActiveRecord::Migration
-  def change
+  def up
     change_column :cuotas, :vencimiento, :date, null: false
     change_column :cuotas, :descripcion, :string, null: false
+  end
+
+  def down
+    change_column :cuotas, :vencimiento, :date, null: true
+    change_column :cuotas, :descripcion, :string, null: true
   end
 end
