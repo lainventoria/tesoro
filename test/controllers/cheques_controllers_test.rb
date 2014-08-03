59f4146575c0de5b702e78a2338bd36bfea930f0
aza <aza@esfriki.com>
2014-08-03 01:54:35 -0300

    corrigiendo el nombre del test

--- test/controllers/cheques_controllers_test.rb
+++ test/controllers/cheques_controllers_test.rb
@@ -53,7 +53,7 @@ class ChequesControllerTest < ActionController::TestCase
     assert_redirected_to obra_cheques_path(@cheque.chequera.obra, situacion: 'propio')
   end
 
-  test "paga sin fondos" do
+  test "paga sin fondos y que falle" do
     @cheque.cuenta = create(:cuenta, :con_fondos, monto: @cheque.monto / 2)
     assert @cheque.save, @cheque.errors.messages
     patch :pagar, obra_id: @cheque.chequera.obra,
