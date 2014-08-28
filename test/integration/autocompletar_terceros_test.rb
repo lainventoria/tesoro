a3a36df31e36f79512f26b1f2f84c3c06547ddc0
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-08-28 19:43:53 -0300

    Extraer código común de terceros en contratos y facturas
    Primera ronda reingenieril

--- test/integration/autocompletar_terceros_test.rb
+++ test/integration/autocompletar_terceros_test.rb
@@ -19,4 +19,18 @@ feature 'Autocompletar terceros' do
       page.must_have_content @tercero.cuit
     end
   end
+
+  scenario 'en las facturas', js: true do
+    visit new_obra_factura_path(create(:obra))
+
+    within 'form' do
+      fill_in('factura_tercero_attributes_nombre', with: @tercero.nombre)
+    end
+
+    find('li.ui-menu-item').click
+
+    within 'form' do
+      page.must_have_content @tercero.cuit
+    end
+  end
 end
