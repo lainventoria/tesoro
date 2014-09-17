210a89acd71a04e7dba478b425b8064d11442fe3
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-17 20:54:14 -0300

    testear una obra vendida

--- test/integration/unidades_funcionales_test.rb
+++ test/integration/unidades_funcionales_test.rb
@@ -13,4 +13,15 @@ feature 'Unidades Funcionales' do
     page.must_have_content @uf.tipo
     page.must_have_content @uf.descripcion
   end
+
+  scenario 'Listar unidades funcionales vendidas' do
+    cv = create :contrato_de_venta
+    cv.agregar_unidad_funcional @uf
+
+    visit obra_unidades_funcionales_path(@obra)
+
+    within "tr[data-uri='#{obra_unidad_funcional_path(@obra, @uf)}']" do
+      page.must_have_link 'vendida', obra_contrato_de_venta_path(@obra, cv)
+    end
+  end
 end
