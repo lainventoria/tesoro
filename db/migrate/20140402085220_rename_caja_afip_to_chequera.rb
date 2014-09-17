159db0db2661cf1f1573b10258c0058eb832b935
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-09-17 20:47:32 -0300

    No se por qué rails usa ALTER TABLE sarasa RENAME INDEX y maria no lo soporta

--- db/migrate/20140402085220_rename_caja_afip_to_chequera.rb
+++ db/migrate/20140402085220_rename_caja_afip_to_chequera.rb
@@ -1,5 +1,7 @@
 class RenameCajaAfipToChequera < ActiveRecord::Migration
   def change
+    remove_index :retenciones, :caja_afip_id
     rename_column :retenciones, :caja_afip_id, :chequera_id
+    add_index :retenciones, :chequera_id
   end
 end
