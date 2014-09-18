e0bab2b8b4b064e8e166f95b646839d8dbe7ea36
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-09-17 23:12:39 -0300

    Reingeniería del test así no usa js

--- test/integration/avisar_que_faltan_indices_test.rb
+++ test/integration/avisar_que_faltan_indices_test.rb
@@ -2,22 +2,16 @@
 require './test/test_helper'
 
 feature 'Avisar que falta el índice de construcción de este mes' do
-  # que al menos haya un indice anterior
-  background { create :indice, :para_cuotas }
-
   scenario 'Todavía no se cargaron los índices del mes' do
+    create :indice, :para_cuotas, periodo: Date.today.last_month.beginning_of_month
     visit root_path
 
     page.must_have_content I18n.t(:avisar_si_faltan_indices)
   end
 
-  scenario 'Ir y cargar el índice', js: true do
-    indice = create :indice, :para_cuotas, temporal: true,
-      periodo: Date.today.beginning_of_month
-
-    visit edit_indice_path(indice)
-
-    find('#btnGuardar').click
+  scenario 'Ya se cargó un índice este mes' do
+    create :indice, :para_cuotas, periodo: Date.today.beginning_of_month
+    visit root_path
 
     page.wont_have_content I18n.t(:avisar_si_faltan_indices)
   end
