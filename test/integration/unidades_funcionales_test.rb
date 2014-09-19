5d4fcff374def18c33ba667e992ce124009c092c
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-19 17:13:12 -0300

    anidame este +1 ♬

--- test/integration/unidades_funcionales_test.rb
+++ test/integration/unidades_funcionales_test.rb
@@ -5,9 +5,6 @@ feature 'Unidades Funcionales' do
   background do
     @obra = create :obra
     @uf = create :unidad_funcional, obra: @obra
-    @cv = create :contrato_de_venta
-    @cv.agregar_unidad_funcional @uf
-    @uf.update_attribute(:precio_venta_final, Money.new(100000, 'ARS'))
   end
 
   scenario 'Listar unidades funcionales' do
@@ -17,18 +14,26 @@ feature 'Unidades Funcionales' do
     page.must_have_content @uf.descripcion
   end
 
-  scenario 'Listar unidades funcionales vendidas' do
-    visit obra_unidades_funcionales_path(@obra)
+  feature 'Vendidas' do
+    background do
+      @cv = create :contrato_de_venta
+      @cv.agregar_unidad_funcional @uf
+      @uf.update_attribute(:precio_venta_final, Money.new(100000, 'ARS'))
+    end
 
-    within "tr[data-uri='#{obra_unidad_funcional_path(@obra, @uf)}']" do
-      page.must_have_link 'vendida', obra_contrato_de_venta_path(@obra, @cv)
+    scenario 'Listar unidades funcionales vendidas' do
+      visit obra_unidades_funcionales_path(@obra)
+
+      within "tr[data-uri='#{obra_unidad_funcional_path(@obra, @uf)}']" do
+        page.must_have_link 'vendida', obra_contrato_de_venta_path(@obra, @cv)
+        page.must_have_content '1.000,00 ARS'
+      end
+    end
+
+    scenario 'Mostrar el precio de venta final si la unidad está vendida' do
+      visit obra_unidad_funcional_path(@obra, @uf)
+
       page.must_have_content '1.000,00 ARS'
     end
-  end
-
-  scenario 'Mostrar el precio de venta final si la unidad está vendida' do
-    visit obra_unidad_funcional_path(@obra, @uf)
-
-    page.must_have_content '1.000,00 ARS'
   end
 end
