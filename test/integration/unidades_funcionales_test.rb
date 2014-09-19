e02e6a6f421e78cc8dda6fc65b7a12f220435903
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-19 10:19:46 -0300

    pasar el contrato a background

--- test/integration/unidades_funcionales_test.rb
+++ test/integration/unidades_funcionales_test.rb
@@ -5,6 +5,9 @@ feature 'Unidades Funcionales' do
   background do
     @obra = create :obra
     @uf = create :unidad_funcional, obra: @obra
+    @cv = create :contrato_de_venta
+    @cv.agregar_unidad_funcional @uf
+    @uf.update_attribute(:precio_venta_final, Money.new(100000, 'ARS'))
   end
 
   scenario 'Listar unidades funcionales' do
@@ -15,25 +18,15 @@ feature 'Unidades Funcionales' do
   end
 
   scenario 'Listar unidades funcionales vendidas' do
-    cv = create :contrato_de_venta
-    cv.agregar_unidad_funcional @uf
-    @uf.precio_venta_final = Money.new(100000, 'ARS')
-    @uf.save
-
     visit obra_unidades_funcionales_path(@obra)
 
     within "tr[data-uri='#{obra_unidad_funcional_path(@obra, @uf)}']" do
-      page.must_have_link 'vendida', obra_contrato_de_venta_path(@obra, cv)
+      page.must_have_link 'vendida', obra_contrato_de_venta_path(@obra, @cv)
       page.must_have_content '1.000,00 ARS'
     end
   end
 
   scenario 'Mostrar el precio de venta final si la unidad está vendida' do
-    cv = create :contrato_de_venta
-    cv.agregar_unidad_funcional @uf
-    @uf.precio_venta_final = Money.new(100000, 'ARS')
-    @uf.save
-
     visit obra_unidad_funcional_path(@obra, @uf)
 
     page.must_have_content '1.000,00 ARS'
