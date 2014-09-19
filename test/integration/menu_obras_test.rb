265044f9b4d3f16a959723dfe35ecdaa1a7d8739
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-19 12:58:26 -0300

    sacar el obra1

--- test/integration/menu_obras_test.rb
+++ test/integration/menu_obras_test.rb
@@ -5,7 +5,7 @@ require './test/test_helper'
 
 feature 'Menu de obras' do
   background do
-    @obra1 = create :obra
+    @obra = create :obra
   end
 
   # Cobros y Pagos
@@ -13,48 +13,48 @@ feature 'Menu de obras' do
     visit pagos_facturas_path
 
     page.must_have_link 'Todas las obras', href: pagos_facturas_path
-    page.must_have_link @obra1.nombre, href: pagos_obra_facturas_path(@obra1)
+    page.must_have_link @obra.nombre, href: pagos_obra_facturas_path(@obra)
   end
 
   scenario 'Los recibos de pago se filtran por obra' do
     visit pagos_recibos_path
 
     page.must_have_link 'Todas las obras', href: pagos_recibos_path
-    page.must_have_link @obra1.nombre, href: pagos_obra_recibos_path(@obra1)
+    page.must_have_link @obra.nombre, href: pagos_obra_recibos_path(@obra)
   end
 
   scenario 'Las facturas de cobro se filtran por obra' do
     visit cobros_facturas_path
 
     page.must_have_link 'Todas las obras', href: cobros_facturas_path
-    page.must_have_link @obra1.nombre, href: cobros_obra_facturas_path(@obra1)
+    page.must_have_link @obra.nombre, href: cobros_obra_facturas_path(@obra)
   end
 
   scenario 'Los recibos de cobro se filtran por obra' do
     visit cobros_recibos_path
 
     page.must_have_link 'Todas las obras', href: cobros_recibos_path
-    page.must_have_link @obra1.nombre, href: cobros_obra_recibos_path(@obra1)
+    page.must_have_link @obra.nombre, href: cobros_obra_recibos_path(@obra)
   end
 
   # Configuración
   scenario 'Las obras se filtran por obra (?)' do
     visit obras_path
     page.must_have_link 'Todas las obras', href: root_path
-    page.must_have_link @obra1.nombre, href: obra_path(@obra1)
+    page.must_have_link @obra.nombre, href: obra_path(@obra)
   end
 
   scenario 'Los índices no se filtran por obra' do
     visit indices_path
 
     page.must_have_link 'Todas las obras', href: indices_path
-    page.must_have_link @obra1.nombre, href: indices_path
+    page.must_have_link @obra.nombre, href: indices_path
   end
 
   scenario 'Los terceros no se filtran por obra' do
     visit terceros_path
 
     page.must_have_link 'Todas las obras', href: terceros_path
-    page.must_have_link @obra1.nombre, href: terceros_path
+    page.must_have_link @obra.nombre, href: terceros_path
   end
 end
