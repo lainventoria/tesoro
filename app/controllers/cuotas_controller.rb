8f147c40f3f37b8a4764fbec7a5b84f0eb8ee373
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-25 15:33:55 -0300

    ordenar y filtrar por fechas
    de paso se muestran las cuotas de este mes por defecto, para reducir la
    cantidad de indices auto generados

--- app/controllers/cuotas_controller.rb
+++ app/controllers/cuotas_controller.rb
@@ -1,13 +1,31 @@
 # encoding: utf-8
 class CuotasController < ApplicationController
   before_action :set_obra
+  before_action :set_order, only: [:index]
 
   def index
     @cuotas = @obra.cuotas
+
+    if params[:date].present?
+      if params[:date][:year].present?
+        # FIXME year() es específico de mysql, pg usa extract(year from
+        # :vencimiento)
+        @cuotas = @cuotas.where('year(vencimiento) = ?', params[:date][:year])
+      end
+
+      if params[:date][:month].present?
+        # FIXME idem anterior
+        @cuotas = @cuotas.where('month(vencimiento) = ?', params[:date][:month].to_i)
+      end
+    else
+      @cuotas = @cuotas.where(vencimiento: Date.today.beginning_of_month)
+    end
+
+    @cuotas = @cuotas.order(@order)
   end
 
   def show
     @editar = false
-    @cuota = (@obra.present? ? @obra.cuotas : Cuota).find(params[:id])
+    @cuota = @obra.cuotas.find(params[:id])
   end
 end
