b28e87f3c5d8fdf4969972c81e2d2029a86fbb9d
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-09-26 17:55:56 -0300

    test del controlador de cuotas

---
+++ test/controllers/cuotas_controller_test.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'test_helper'
+
+class CuotasControllerTest < ActionController::TestCase
+  setup do
+    @cuota = create :cuota
+  end
+
+  test 'accede a la lista de cuotas' do
+    get :index, obra_id: @cuota.contrato_de_venta.obra
+    assert_response :success
+    assert_not_nil assigns(:cuotas)
+  end
+
+  test 'muestra' do
+    get :show, obra_id: @cuota.contrato_de_venta.obra, id: @cuota
+    assert_response :success
+  end
+
+  test 'genera factura' do
+    get :generar_factura, obra_id: @cuota.contrato_de_venta.obra, id: @cuota
+
+    @cuota.reload
+
+    assert_redirected_to obra_factura_path(@cuota.contrato_de_venta.obra, @cuota.factura)
+    assert_instance_of Factura, @cuota.factura
+  end
+end
